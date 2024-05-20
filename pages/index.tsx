aa8d3098a2ebedf80fcd8c8babcc6025c1cdba3e
aqpmzngldh <124235388+aqpmzngldh@users.noreply.github.com>
2024-05-20 11:31:29 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,7 +93,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         </div>
       </main>
       <footer className="p-6 text-center text-white/80 sm:p-12">
-        Thank you to{' '}
+{/*         Thank you to{' '} */}
 {/*         <a
           href="https://vercel.com/cherrylover/notion-b/"
           target="_blank"
